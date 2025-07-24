cbaf65882b9504f84b9fda2d2b2a24081b78023b
Usman <razausmankhan97@gmail.com>
2025-07-23 22:00:39 -0400

    location chunking code

--- app/routers/data_collection.py
+++ app/routers/data_collection.py
@@ -15,3 +15,32 @@ def get_data():
     data = response.json()
         
     return {"response": data}
+
+
+43.4767105,-79.6323979
+43.6074866,-79.8083237
+43.6311091,-79.6937843
+43.6285557,-79.6764448
+
+min_lat= 43.47
+max_lat = 43.63
+
+min_long = -79.81
+max_long = -79.63
+
+@router.get('/get-locations')
+def get_location(): 
+    locations = [ ]
+   
+    step_size = 0.01 
+    lat_length = int((max_lat - min_lat)/step_size) + 1 
+    long_length = int((max_long - min_long)/step_size) + 1 
+    
+    for i in range(lat_length): 
+        lat =  round(min_lat + (i * step_size),2)
+        for j in range(long_length): 
+            long = round(min_long + ( j * step_size),2)
+            locations.append((lat,long)) 
+    
+    return {"repsonse" : [lat_length, long_length], 
+            "locations" : locations }
